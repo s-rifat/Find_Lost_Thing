79fa168f519c968e1af0e45dabdefe50592884b2
Syed Rifat <gtr06.rft@gmail.com>
2020-09-23 19:13:05 +0600

    commit 3

--- FindLostThings/FindLostThings/Controllers/AccountController.cs
+++ FindLostThings/FindLostThings/Controllers/AccountController.cs
@@ -100,119 +100,166 @@ namespace FindLostThings.Controllers
 
             return View();
         }
-      
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Account account = db.Accounts.Find(id);
+            if (account == null)
+            {
+                return HttpNotFound();
+            }
+            return View(account);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Account account = db.Accounts.Find(id);
+            if (account == null)
+            {
+                return HttpNotFound();
+            }
+            return View(account);
+        }
+
+        // POST: Account/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Account account = db.Accounts.Find(id);
+            db.Accounts.Remove(account);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
 
 
 
-        /*  / 
-       // GET: Account
-       public ActionResult Index()
-       {
-           return View(db.Accounts.ToList());
-       }
-  /*
-        // GET: Account/Details/5
-       public ActionResult Details(int? id)
-       {
-           if (id == null)
-           {
-               return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-           }
-           Account account = db.Accounts.Find(id);
-           if (account == null)
-           {
-               return HttpNotFound();
-           }
-           return View(account);
-       }
-          // GET: Account/Create
-          public ActionResult Create()
-          {
-              return View();
-          }
+        /*
+     // GET: Account
+     public ActionResult Index()
+     {
+         return View(db.Accounts.ToList());
+     }
+/*
+      // GET: Account/Details/5
+     public ActionResult Details(int? id)
+     {
+         if (id == null)
+         {
+             return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+         }
+         Account account = db.Accounts.Find(id);
+         if (account == null)
+         {
+             return HttpNotFound();
+         }
+         return View(account);
+     }
+        // GET: Account/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
 
-          // POST: Account/Create
-          // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-          // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-          [HttpPost]
-          [ValidateAntiForgeryToken]
-          public ActionResult Create([Bind(Include = "userId,userName,password,phoneNumber")] Account account)
-          {
-              if (ModelState.IsValid)
-              {
-                  db.Accounts.Add(account);
-                  db.SaveChanges();
-                  return RedirectToAction("Index");
-              }
+        // POST: Account/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "userId,userName,password,phoneNumber")] Account account)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Accounts.Add(account);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
 
-              return View(account);
-          }
+            return View(account);
+        }
 
-          // GET: Account/Edit/5
-          public ActionResult Edit(int? id)
-          {
-              if (id == null)
-              {
-                  return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-              }
-              Account account = db.Accounts.Find(id);
-              if (account == null)
-              {
-                  return HttpNotFound();
-              }
-              return View(account);
-          }
+        // GET: Account/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Account account = db.Accounts.Find(id);
+            if (account == null)
+            {
+                return HttpNotFound();
+            }
+            return View(account);
+        }
 
-          // POST: Account/Edit/5
-          // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-          // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-          [HttpPost]
-          [ValidateAntiForgeryToken]
-          public ActionResult Edit([Bind(Include = "userId,userName,password,phoneNumber")] Account account)
-          {
-              if (ModelState.IsValid)
-              {
-                  db.Entry(account).State = EntityState.Modified;
-                  db.SaveChanges();
-                  return RedirectToAction("Index");
-              }
-              return View(account);
-          }
+        // POST: Account/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "userId,userName,password,phoneNumber")] Account account)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(account).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(account);
+        }
 
-          // GET: Account/Delete/5
-          public ActionResult Delete(int? id)
-          {
-              if (id == null)
-              {
-                  return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-              }
-              Account account = db.Accounts.Find(id);
-              if (account == null)
-              {
-                  return HttpNotFound();
-              }
-              return View(account);
-          }
+        // GET: Account/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Account account = db.Accounts.Find(id);
+            if (account == null)
+            {
+                return HttpNotFound();
+            }
+            return View(account);
+        }
 
-          // POST: Account/Delete/5
-          [HttpPost, ActionName("Delete")]
-          [ValidateAntiForgeryToken]
-          public ActionResult DeleteConfirmed(int id)
-          {
-              Account account = db.Accounts.Find(id);
-              db.Accounts.Remove(account);
-              db.SaveChanges();
-              return RedirectToAction("Index");
-          }
+        // POST: Account/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Account account = db.Accounts.Find(id);
+            db.Accounts.Remove(account);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
 
-          protected override void Dispose(bool disposing)
-          {
-              if (disposing)
-              {
-                  db.Dispose();
-              }
-              base.Dispose(disposing);
-          }*/
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }*/
 
 
     }
